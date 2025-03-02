0a17da2ee82e9247dba3dac208db7ba1ba59637f
Begenc Haciyev <begahaciyew@gmail.com>
2025-03-02 21:47:27 +0300

    auth api creating

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  // Set the global prefix to 'api'
   app.setGlobalPrefix('api')
-  app.enableCors()
-  await app.listen(4200)
+	app.enableCors()
+  
+	await app.listen(4200)
+
 }
-bootstrap()
+bootstrap();
